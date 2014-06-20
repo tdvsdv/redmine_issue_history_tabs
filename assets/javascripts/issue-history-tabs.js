8b0471f1c5d95f34ad92ec31d8767868be1bf87b
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-06-20 12:50:53 +1100

    #20542 done. Added files visibility on comment tab

--- assets/javascripts/issue-history-tabs.js
+++ assets/javascripts/issue-history-tabs.js
@@ -1,33 +1,49 @@
-function show_comments() {
-  $('.journal').hide();
-  $('.journal.has-notes').show();
-  $('.journal.has-notes ul.details').hide();
-}
+RMPlus.TABS = (function (my) {
+  var my = my || {};
 
-function show_all() {
-  $('.journal').show();
-  $('.journal.has-notes ul.details').show();
-}
+  my.show_comments = function () {
+    $('.journal').each(function (index) {
+      var $journal = $(this);
+      $journal.hide();
+      if ($journal.hasClass('has-notes'))  {
+        $journal.show();
+      }
+      if ($journal.find('a[href^="/attachments"]').length > 0) {
+        $journal.show();
+        $journal.find('.details li').hide();
+        $journal.find('li:has(a[href^="/attachments"])').show();
+      } else {
+        $journal.find('ul.details').hide();
+      }
+    });
+  };
 
+  my.show_all = function () {
+    $('.journal').show().find('.details').show().find('li').show();
+  };
+
+  return my;
+})(RMPlus.TABS || {});
 
 $(document).ready(function(){
 
-  $(document.body).on("click", "#tab-history_comments", function(){
-    $(this).addClass("selected");
-    $('#tab-history_all').removeClass("selected");
-    show_comments();    
-    return false;
-  });  
-
-  $(document.body).on("click", "#tab-history_all", function(){
-    $(this).addClass("selected");
-    $("#tab-history_comments").removeClass("selected");
-    show_all();
-    return false;
+  $(document.body).on("click", "#tab-history_comments", function() {
+    RMPlus.TABS.show_comments();
   });
 
-  $("#tab-history_comments").hasClass('selected') ? show_comments() : show_all();
+  $(document.body).on("click", "#tab-history_all", function() {
+    RMPlus.TABS.show_all();
+  });
 
-  ( $("#history").length ) ?  $("#history").prepend($("#history_tabs") ) : $("#history_tabs").hide();  
+  $("#tab-history_comments").hasClass('selected') ? RMPlus.TABS.show_comments() : RMPlus.TABS.show_all();
 
+  if ($("#history").length > 0) {
+    $("#history_tabs").prependTo($("#history"));
+    // var $journals = $('.journal');
+    // $journals.appendTo('#tab-content-history_all');
+    // $('.journal.has-notes').clone().appendTo('#tab-content-history_comments').find('ul.details').remove();
+  }
+  else {
+    $("#history_tabs").hide();
+  }
 });
